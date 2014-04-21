095cc9ff753529cad8b493b2df083eb93c45d802
Sumit Singh <sumsingh.com>
2014-04-21 13:59:04 +0530

    hrtimer: enhance power efficiency
    Defining relaxed version of hrtimer_callback_running(),
    which will be used to improve power efficiency through
    the use of macro cpu_relaxed_read_long.
    
    Bug 1440421
    
    Change-Id: Ie42d7ae9628a817d52f4636781e11b607327c2c5
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/398789
    (cherry picked from commit 0d9f5fc1d39d7d1809519b5d11bf7ac72287b7c6)
    Reviewed-on: http://git-master/r/422255
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/linux/hrtimer.h
+++ include/linux/hrtimer.h
@@ -421,6 +421,11 @@ static inline int hrtimer_callback_running(struct hrtimer *timer)
 	return hrtimer_callback_running_relaxed(timer);
 }
 
+static inline int hrtimer_callback_running_relaxed(struct hrtimer *timer)
+{
+	return cpu_relaxed_read_long(&timer->state) & HRTIMER_STATE_CALLBACK;
+}
+
 /* Forward a hrtimer so it expires after now: */
 extern u64
 hrtimer_forward(struct hrtimer *timer, ktime_t now, ktime_t interval);
