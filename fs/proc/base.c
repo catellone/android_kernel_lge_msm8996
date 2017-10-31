785374981dd2e25c5b3d53196677a6b928c3ab18
Marissa Wall <marissaw@google.com>
2017-10-31 15:05:13 -0700

    ANDROID: cpufreq: concurrent_active_time by pid
    In order to model the energy used by the cpu,
    we need to expose cpu time information to
    userspace. Output the time each pid spent running
    with 1 to num_possible_cpus online and not idle.
    
    We can use this information to blame pids and
    uids for the energy they are using.
    
    For instance, if 4 cores are online and active
    for 50ms and the pid is running on one of the
    cores, the pid should be blamed for 1/4 of the
    base energy used by the cpu when 1 or more cores is
    active. Using other proc files, the pid can
    also be blamed for the cost of keeping a cluster
    on and the cost of the individual cores it
    is using.
    
    b/68399339
    
    Test: cat /proc/<PID>/concurrent_active_time
    
    Change-Id: Iaecf4ca71e7043a9c7b97787000a74a514eb9f04
    Signed-off-by: Marissa Wall <marissaw@google.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2337,7 +2337,7 @@ out:
 	return -ENOENT;
 }
 
-static struct dentry *proc_pident_lookup(struct inode *dir, 
+static struct dentry *proc_pident_lookup(struct inode *dir,
 					 struct dentry *dentry,
 					 const struct pid_entry *ents,
 					 unsigned int nents)
@@ -2482,7 +2482,7 @@ static const struct pid_entry attr_dir_stuff[] = {
 
 static int proc_attr_dir_readdir(struct file *file, struct dir_context *ctx)
 {
-	return proc_pident_readdir(file, ctx, 
+	return proc_pident_readdir(file, ctx,
 				   attr_dir_stuff, ARRAY_SIZE(attr_dir_stuff));
 }
 
@@ -2906,6 +2906,10 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_CHECKPOINT_RESTORE
 	REG("timers",	  S_IRUGO, proc_timers_operations),
 #endif
+#ifdef CONFIG_CPU_FREQ_STAT
+	ONE("time_in_state", 0444, proc_time_in_state_show),
+	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
+#endif
 };
 
 static int proc_tgid_base_readdir(struct file *file, struct dir_context *ctx)
@@ -3284,6 +3288,10 @@ static const struct pid_entry tid_base_stuff[] = {
 	REG("gid_map",    S_IRUGO|S_IWUSR, proc_gid_map_operations),
 	REG("projid_map", S_IRUGO|S_IWUSR, proc_projid_map_operations),
 	REG("setgroups",  S_IRUGO|S_IWUSR, proc_setgroups_operations),
+#endif
+#ifdef CONFIG_CPU_FREQ_STAT
+	ONE("time_in_state", 0444, proc_time_in_state_show),
+	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
 #endif
 };
 
